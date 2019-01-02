7bda654471ca5ab508cd8c3c78fe0f60c997871c
Sara Ebbers <johndoe@example.com>
2019-01-02 16:40:48 -0700

    Set up file structure, connect scss, add set up test file, git ignore apiKey and storeApiKey

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../index.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Just a Bill</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
