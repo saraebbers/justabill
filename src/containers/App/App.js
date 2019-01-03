290a37f6b60cfe701ed5a8a80e44a3c8403dba8b
Sara Ebbers <johndoe@example.com>
2019-01-03 11:48:59 -0700

    Install Router, add routes for navigation, build structure of routed containers

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import '../../index.scss';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <h1>Just a Bill</h1>
+        <div className='header-section'>
+          <header>
+            <h1>Just a Bill</h1>
+          </header>
+        </div>
+        <div className='nav-section'>
+          <nav>
+            <NavLink to='/bills' component={Bills} className='nav'>Bills</NavLink>
+            <NavLink to='/lessons' component={Lessons} className='nav'>Lesson Plans</NavLink>
+            <NavLink to='/resources' component={Resources} className='nav'>Resources</NavLink>
+          </nav>
+        </div>
       </div>
     );
   }
