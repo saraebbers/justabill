fbd3e75eb127c8059c373cbba96ef8c264b44647
Sara Ebbers <johndoe@example.com>
2019-01-04 09:55:16 -0700

    Add display component that will contain both bills and favorites, update paths, style display component

---
+++ src/components/Display/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../index.scss';
+
+const Display = () => {
+
+  return(
+    <div className='display'>
+    This is the Display Area
+    </div>
+
+  )
+}
+
+export default Display;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,7 +5,7 @@ import Welcome from '../../components/Welcome/Welcome';
 import Bills from '../Bills/Bills';
 import Lessons from '../Lessons/Lessons';
 import Favorites from '../Favorites/Favorites';
-
+import Display from '../../components/Display/Display';
 
 class App extends Component {
 
@@ -19,14 +19,12 @@ class App extends Component {
         </Link>
         <div className='nav-section'>
           <NavLink to='/bills' className='nav'>Bills</NavLink>
-          <NavLink to='/lessons' className='nav'>Lesson Plans</NavLink>
           <NavLink to='/favorites' className='nav'>Favorites</NavLink>
         </div>
         <Switch>
           <Route exact path='/' component={Welcome} />
-          <Route exact path='/bills' component={Bills} />
-          <Route exact path='/lessons' component={Lessons} />
-          <Route exact path='/favorites' component={Favorites} />
+          <Route exact path='/bills' render={() => <Display type="Bills" />} />
+          <Route exact path='/favorites' render={() => <Display type="Favorites" />} />
         </Switch>
       </div>
     );
